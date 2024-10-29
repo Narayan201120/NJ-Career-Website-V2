da942d94d617dff66370c0756ac6c2fb51d97d82
Narayan201120 <159036898+Narayan201120@users.noreply.github.com>
2024-10-29 15:19:58 +0000

    changes

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ JOBS = [
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html', jobs=JOBS, company_name='Jovian')
+    return render_template('home.html', jobs=JOBS, company_name='Basic')
 
 @app.route('/api/jobs')
 def list_jobs():
